3822f8878aa9695435569f7fe33cb8c36dc37d6c
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-01-08 09:36:45 +0100

    starting from almost scratch

--- script.js
+++ script.js
@@ -1,47 +1,22 @@
-/* becode/javascript
- *
- * /07-misc/07-storage-clicker/script.js - 7.7: jeu : clicker persistant
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 let upgradeCost = document.querySelectorAll(".cost");
 let counter = document.querySelector("#counter");
-console.log();
 
-(function() {
-    var Buttons = document.querySelectorAll(".upgradeBtn");
-    console.log(Buttons);
+function Cookie(clicks, autoincrease) {
 
-    document.querySelectorAll(".upgradeBtn").forEach(function (btn) {
-        btn.addEventListener("click", function () {
-            switch (btn) {
-                case upgradeOne:
-                    upgradeCost[0].innerHTML = Math.floor(Number(upgradeCost[0].innerHTML)*1.1);
-                    if (counter.innerHTML > upgradeCost[0]){
-                        var localStorage = localStorage.getItem("lastCount");
-                        var newCount = localStorage - upgradeCost[0];
-                        console.log(newCount);
-                        counter.innerHTML = newCount;
-                    }
-                    break;
-                case upgradeTwo:
-                    upgradeCost[1].innerHTML = Math.floor(Number(upgradeCost[1].innerHTML)*1.1);
-                    break;
-                case upgradeThree:
-                    upgradeCost[2].innerHTML = Math.floor(Number(upgradeCost[2].innerHTML)*1.1);
-                    break;
-                case upgradeFour:
-                    upgradeCost[3].innerHTML = Math.floor(Number(upgradeCost[3].innerHTML)*1.1);
-                    break;
-            }
+    this.clicks = clicks;
+    this.autoincrease = autoincrease;
 
-        })
-    });
+    this.autoIncrease = function test() {
 
+        this.amount = this.amount * + 1 * autoincrease;
+        setInterval()
+
+    }
+
+}
+
+new Cookie(0, 0);
 
 
     let image = document.getElementById("clickImage");
