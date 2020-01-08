00d602e900454a31a397f71d92810785fba4b913
Becode <stevekeersma@gmail.com>
2020-01-08 16:18:06 +0100

    split off in extra JS

--- script.js
+++ script.js
@@ -1,73 +1,15 @@
 
+
+let cookie1 = new Cookie();
+
+
 let upgradeCost = 0;
 let counter = document.querySelector("#counter");
 let clickAmount = 0;
 let intervalIncrease = 0;
 let totalAmaount = 0;
 
-
-
-//upgrade
-function oneUp() {
-        if (totalAmaount >= 10)
-        {
-            intervalIncrease++;
-
-
-            updateVegan();
-        }
-        return intervalIncrease;
-
-}
-
 let upgradeOne = document.getElementById("upgradeOne");
-
-
-
-
-
-
-function Cookie() {
-
-
-    this.click = function () {
-
-        clickAmount++;
-        updateVegan();
-
-        return totalAmaount;
-
-    };
-
-    this.autoIncrease = function () {
-        if (totalAmaount - upgradeCost <= 0){
-            console.log("fuck off mate");
-        } else {
-            setInterval(oneUp, 1000);
-            upgradeCost += 10;
-            updateVegan();
-
-
-            return totalAmaount;
-        }
-
-    this.update = function () {
-
-    }
-    };
-
-
-}
-
-let cookie1 = new Cookie();
-
-
-
-
-
-
-
-
 
 
 // cookie clickable and saves number
